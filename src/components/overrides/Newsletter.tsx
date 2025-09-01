62d3228525101b8f2b6395bc98351b617857231e
Bruno Martins <brunomart97@hotmail.com>
2025-09-01 20:33:19 -0300

    feat: newsletter style override

---
+++ src/components/overrides/Newsletter.tsx
@@ -0,0 +1,9 @@
+import { SectionOverride } from "@faststore/core";
+import styles from "../../sass/index.module.scss";
+
+const override: SectionOverride = {
+  section: "Newsletter",
+  className: styles.myNewsletterClass,
+};
+
+export { override };
